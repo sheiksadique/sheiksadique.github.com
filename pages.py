7cbcd0d4720bd0f09c2baafe0d69d29fdc112d1f
Sadique Sheik <sheiksadique@gmail.com>
2012-12-08 22:04:37 +0100

    Added howto page.

--- pages.py
+++ pages.py
@@ -20,14 +20,34 @@ def generatePage(page='index.html', path='./src/pages/', templates='./src/templa
     destination: location of generated files
     '''
     pg = parse(path+page)
-    tempNodes = pg.getElementsByTagName('template')
-    for tn in tempNodes:
-        tp = getTemplate(tn.getAttribute('class'), templates=templates)
-        tn.parentNode.replaceChild(tp, tn)
+    #tempNodes = pg.getElementsByTagName('template')
+    #for tn in tempNodes:
+    #    tp = getTemplate(tn.getAttribute('class'), templates=templates)
+    #    tn.parentNode.replaceChild(tp, tn)
+    includeTemplate(pg, templates=templates)
     f = open(destination+page, 'w')
     pg.writexml(f)
     f.close()
     return pg
+
+def includeTemplate(pg, templates='./src/templates/'):
+    '''
+    Include/replace html code from templates recursively
+    pg: parse html page/node
+    templates: location of template files
+    '''
+    tempNodes = pg.getElementsByTagName('template')
+    for tn in tempNodes:
+        tp = getTemplate(tn.getAttribute('class'), templates=templates)
+        tn.parentNode.replaceChild(tp, tn)
+    
+    # Check for templates 
+    tempNodes = pg.getElementsByTagName('template')
+    if len(tempNodes) > 0:
+        includeTemplate(pg, templates=templates)
+
+    return pg
+
 
 def getTemplate(name='header', templates='./src/templates/'):
     '''
@@ -36,4 +56,5 @@ def getTemplate(name='header', templates='./src/templates/'):
     pg = parse(templates+name+'.tmpl')
     return pg.childNodes[0]
 
-generateAllPages(destination='./')
+if __name__=='__main__':
+    generateAllPages(destination='./')
