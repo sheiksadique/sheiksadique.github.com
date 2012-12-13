1ba9985f6ffedcac035d938be21b0316b6fb67ae
Sadique Sheik <sheiksadique@gmail.com>
2012-12-13 03:36:52 +0100

    AJAX based post display

--- README.rst
+++ README.rst
@@ -25,12 +25,38 @@ easier. The directory structure is as follows.
 
     img/ -- Image directory
     styles/ -- css style sheet files
+    posts/ -- individual posts
     src/ -- This is where the webpages under development are placed.
                   There are essentially two calsses of files.
                   /pages/ - .html : HTML page files.
                   /templates/ - .tmpl : reusable html code.
 
 Any pages that are to be included should be named \*.html and placed in templates folder. You can use "<template class='templatename'/>" tag in your pages to use any of the reusable html in src/templates/\*.tmpl files.
+
+Feature
+=======
+
+As of now the only feature that might be useful is to automatically include
+posts that you place in the posts folder with the use of <postListBox /> tag in
+any of your src/pages/ html files.
+
+
+<postListBox /> tag can be placed in the pages to automatically import all the
+available posts and be listed in a list on the page (checkout the tips-n-tricks
+page for an example of this).
+
+The final page uses AJAX to display posts when you select any of of them from
+the list.
+
+Software requirements
+=====================
+
+The final pages are generated here using python code. You will require the
+following packages.
+
+ - python
+ - python-lxml
+
 
 Page generation
 ===============
--- pages.py
+++ pages.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python
 
-from xml.dom.minidom import *
+from lxml import etree
 import glob
 
-def generateAllPages(path='./src/pages/', templates='./src/templates/', destination='./_html/'):
+def generateAllPages(path='./src/pages/', templates='./src/templates/',
+                     posts='./posts/', destination='./_html/'):
     '''
     Function to generate all pages of the website
     '''
     print('The following files are being generated.')
     for p in glob.glob1(path, '*.html'):
         print p
-        generatePage(p, path=path, templates=templates, destination=destination)
+        generatePage(p, path=path, templates=templates, posts=posts,
+                     destination=destination)
 
-def generatePage(page='index.html', path='./src/pages/', templates='./src/templates/', destination='./_html/'):
+def generatePage(page='index.html', path='./src/pages/',
+                 templates='./src/templates/', posts='./posts/',
+                 destination='./_html/',):
     '''
     Generate Page by filling up the templates
     page: name of the page
     templates: location of templates folder
     destination: location of generated files
     '''
-    pg = parse(path+page)
-    #tempNodes = pg.getElementsByTagName('template')
-    #for tn in tempNodes:
-    #    tp = getTemplate(tn.getAttribute('class'), templates=templates)
-    #    tn.parentNode.replaceChild(tp, tn)
+    pg = etree.parse(path+page).getroot()
     includeTemplate(pg, templates=templates)
-    f = open(destination+page, 'w')
-    pg.writexml(f)
-    f.close()
+    for nd in pg.findall('.//{'+pg.nsmap[None]+'}postListBox'):
+        listPosts(nd, posts=posts)
+    with open(destination+page, 'w') as f:
+        f.write(etree.tostring(pg, pretty_print=True))
     return pg
 
 def includeTemplate(pg, templates='./src/templates/'):
@@ -36,25 +37,52 @@ def includeTemplate(pg, templates='./src/templates/'):
     pg: parse html page/node
     templates: location of template files
     '''
-    tempNodes = pg.getElementsByTagName('template')
+    tempNodes = pg.findall('.//{'+pg.nsmap[None]+'}template')
     for tn in tempNodes:
-        tp = getTemplate(tn.getAttribute('class'), templates=templates)
-        tn.parentNode.replaceChild(tp, tn)
+        tp = getTemplate(tn.get('class'), templates=templates)
+        tn.getparent().replace(tn, tp)
     
     # Check for templates 
-    tempNodes = pg.getElementsByTagName('template')
+    tempNodes = pg.findall('.//{'+pg.nsmap[None]+'}template')
     if len(tempNodes) > 0:
         includeTemplate(pg, templates=templates)
 
     return pg
 
-
 def getTemplate(name='header', templates='./src/templates/'):
     '''
     Returns a node with the template html
     '''
-    pg = parse(templates+name+'.tmpl')
-    return pg.childNodes[0]
+    pg = etree.parse(templates+name+'.tmpl')
+    return pg.getroot()
+
+def listPosts(pg, posts='./posts/'):
+    '''
+    Add all posts to the list.
+    pg: node containing the template where posts are to be included
+    posts: Location where posts are found
+    '''
+    print('The following posts are being listed.')
+    node = etree.XML("<div class='postListBox'><div><ul /></div></div>")
+    lst = node.findall('.//ul')[0]
+    for p in glob.glob1(posts, '*.post'):
+        print p
+        lnk = etree.XML('<a />')
+        pst = etree.parse(posts+p).getroot()
+        for div in pst.findall('.//div'):
+            if div.get('class') == 'contentTitle':
+                nd = div.findall('.//a')[0]
+                lnk.set('href', '#'+nd.get('id'))
+                lnk.set('onclick', 
+                        "loadPost('{0}', '#post-container')".format(
+                            nd.get('id')))
+                lnk.text = nd.text
+        itm = etree.XML('<li />')
+        itm.append(lnk)
+        lst.append(itm)
+    pg.getparent().replace(pg, node)
+    return
+
 
 if __name__=='__main__':
     generateAllPages(destination='./')
